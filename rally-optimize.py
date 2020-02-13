40d303ecddbaaad3855c63d6dbd1f47d7aad4208
aggixx <aggixx.gaming@gmail.com>
2020-02-12 22:54:55 -0600

    Account for stone/wood bias in scoring.

--- rally-optimize.py
+++ rally-optimize.py
@@ -51,15 +51,17 @@ class Resource(Enum):
 	def short(self):
 		return self.name[:1]
 
+class CacheEntry:
+	def __init__(self, score, bias):
+		self.score = score
+		self.bias = bias
+
 class Hand:
 	def __init__(self, app, cards):
 		self.cards = cards
 		self.app = app
 
-	def get_score(self):
-		if repr(self.cards) in self.app.hand_cache:
-			return self.app.hand_cache[repr(self.cards)]
-
+	def calc_score(self):
 		hand_resources = []
 
 		for numElements in range(1,5):
@@ -73,10 +75,54 @@ class Hand:
 
 			hand_resources.append(mean(tier_resources))
 
-		score = mean(hand_resources)
-		self.app.hand_cache[repr(self.cards)] = score
+		return mean(hand_resources)
 
-		return score
+	def calc_bias(self):
+		hand_bias = []
+
+		for numElements in range(1,5):
+			tier_bias = []
+
+			for elements in itertools.combinations_with_replacement(Element, numElements):
+				boss = Boss(elements)
+
+				try:
+					max_wood = max(map(lambda h: boss.calculateResources(h), filter(lambda c: c.resource == Resource.WOOD, self.cards)))
+				except ValueError:
+					max_wood = 0
+
+				try:
+					max_stone = max(map(lambda h: boss.calculateResources(h), filter(lambda c: c.resource == Resource.STONE, self.cards)))
+				except ValueError:
+					max_stone = 0
+
+				#logging.info(max_wood)
+				#logging.info(max_stone)
+
+				bias = max_wood - max_stone
+
+				logging.debug("Bias: {}".format(bias))
+
+				tier_bias.append(bias)
+
+			hand_bias.append(mean(tier_bias))
+
+		return mean(hand_bias)
+
+	def cache(self):
+		self.app.hand_cache[repr(self.cards)] = CacheEntry(self.calc_score(), self.calc_bias())
+
+	def get_score(self):
+		if repr(self.cards) not in self.app.hand_cache:
+			self.cache()
+
+		return self.app.hand_cache[repr(self.cards)].score
+
+	def get_bias(self):
+		if repr(self.cards) not in self.app.hand_cache:
+			self.cache()
+
+		return self.app.hand_cache[repr(self.cards)].bias
 
 class Deck:
 	def __init__(self, app, cards):
@@ -103,27 +149,33 @@ class Deck:
 			logging.debug("Scoring {}...".format(self))
 
 			deck_resources = []
+			deck_bias = []
 
 			for hand_cards in itertools.combinations(self.cards, 3):
 				hand = Hand(self.app, hand_cards)
 				deck_resources.append(hand.get_score())
+				deck_bias.append(hand.get_bias())
 
-			self.score = mean(deck_resources)
-			logging.debug("Deck average: {}".format(self.score))
+			self.base_score = mean(deck_resources)
+			self.bias = mean(deck_bias)
+			self.score = self.base_score - abs(self.bias) / 2
+			logging.debug("Deck base score: {}".format(self.base_score))
+			logging.debug("Deck bias: {}".format(self.bias))
+			logging.debug("Deck score: {}".format(self.score))
 
 		return self.score
 
 class Card:
-	def __init__(self, elements, resource_element, resource_amount):
+	def __init__(self, elements, resource, resource_amount):
 		self.elements = elements
 		self.resource_amount = resource_amount
-		self.resource_element = resource_element
+		self.resource = resource
 
 	def __str__(self):
-		return "Card-{}-{}{}".format("".join(map(lambda e: e.short(), self.elements)), self.resource_element.short(), self.resource_amount)
+		return "Card-{}-{}{}".format("".join(map(lambda e: e.short(), self.elements)), self.resource.short(), self.resource_amount)
 
 	def __repr__(self):
-		return "<Card-{}-{}{}>".format("".join(map(lambda e: e.short(), self.elements)), self.resource_element.short(), self.resource_amount)
+		return "<Card-{}-{}{}>".format("".join(map(lambda e: e.short(), self.elements)), self.resource.short(), self.resource_amount)
 
 class Boss:
 	def __init__(self, elements):
@@ -150,7 +202,7 @@ class Boss:
 
 		amount = len(matching) * card.resource_amount
 
-		logging.debug("Resource amount: {} {}".format(amount, card.resource_element))
+		logging.debug("Resource amount: {} {}".format(amount, card.resource))
 
 		return amount
 
@@ -174,11 +226,11 @@ class AppState:
 				elements = list(map(lambda e: Element[e], fields[0]))
 				logging.debug("Elements: {}".format(elements))
 
-				resource_element = Resource[fields[1][:1]]
+				resource = Resource[fields[1][:1]]
 				resource_amount = int(fields[1][1:2])
-				logging.debug("Resource: {} {}".format(resource_amount, resource_element))
+				logging.debug("Resource: {} {}".format(resource_amount, resource))
 
-				cards.append(Card(elements, resource_element, resource_amount))
+				cards.append(Card(elements, resource, resource_amount))
 
 		self.deck = Deck(self, cards)
 
