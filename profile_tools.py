c546dd9132f940ecac3c8076adc1b8b2cb1fd867
aggixx <aggixx.gaming@gmail.com>
2020-02-16 12:54:32 -0600

    Improve profiling tools output

--- profile_tools.py
+++ profile_tools.py
@@ -1,6 +1,7 @@
 import inspect
 import logging
 import time
+import statistics as stats
 
 cumulative_data = {}
 method_names = {}
@@ -35,18 +36,24 @@ def profile_cumulative(f):
 	        else:
 	        	name = f.__name__
 
-        	cumulative_data[f.__name__] = 0
+        	cumulative_data[f.__name__] = []
         	method_names[f.__name__] = name
 
-        cumulative_data[f.__name__] += end-start
+        cumulative_data[f.__name__].append(end-start)
 
         return result
 
     return f_timer
 
 def log_digest():
+    logging.info("       \tSUM\tMEAN\tMEDIAN\tMIN\tMAX\tCOUNT\tDESC")
+    logging.info("=========================================================================================")
+
     for entry in cumulative_data.items():
-        logging.info("Total time spent on '{}': {:.3f}s".format(method_names[entry[0]], entry[1]))
+        key = entry[0]
+        vals = entry[1]
+        #logging.info("Total time spent on '{}': {:.3f}s".format(method_names[key], vals))
+        logging.info("Profile\t{:.3f}s\t{:.3f}s\t{:.3f}s\t{:.3f}s\t{:.3f}s\t{}\t{}".format(sum(vals), stats.mean(vals), stats.median(vals), min(vals), max(vals), len(vals), method_names[key]))
 
 class ChunkProfiler(object):
     def __init__(self, desc):
@@ -59,7 +66,7 @@ class ChunkProfiler(object):
         duration = time.time() - self.start
 
         if self.desc not in cumulative_data:
-            cumulative_data[self.desc] = 0
+            cumulative_data[self.desc] = []
             method_names[self.desc] = "chunk-{}".format(self.desc)
 
-        cumulative_data[self.desc] += duration
+        cumulative_data[self.desc].append(duration)
